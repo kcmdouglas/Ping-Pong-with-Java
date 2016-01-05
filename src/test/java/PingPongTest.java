c8ebae6aea13696302652b571b00946f2464e66d
Kassidy Douglas <kcmdouglas@gmail.com>
2016-01-05 06:15:51 -0800

    Add code to test to fail test 4.

--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -37,4 +37,27 @@ public class PingPongTest {
     assertEquals(expectedArray, testPingPong.isPingPong(6));
   }
 
+  @Test
+  public void isPingPong_forNumbersDivisibleBy15_returnsPingPong() {
+    PingPong testPingPong = new PingPong();
+    ArrayList<Object> expectedArray = new ArrayList<Object>();
+     expectedArray.add(1);
+     expectedArray.add(2);
+     expectedArray.add("ping");
+     expectedArray.add(4);
+     expectedArray.add("pong");
+     expectedArray.add("ping");
+     expectedArray.add(7);
+     expectedArray.add(8);
+     expectedArray.add("ping");
+     expectedArray.add("pong");
+     expectedArray.add(11);
+     expectedArray.add("ping");
+     expectedArray.add(13);
+     expectedArray.add(14);
+     expectedArray.add("pingpong");
+     expectedArray.add(16);
+    assertEquals(expectedArray, testPingPong.isPingPong(16));
+  }
+
 }
