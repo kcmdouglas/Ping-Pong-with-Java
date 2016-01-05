a7292daefa5e25c55bef677abb1e73e097f65b9b
Kassidy Douglas <kcmdouglas@gmail.com>
2016-01-05 06:13:23 -0800

    Add code to PingPong to pass test 3.

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -13,8 +13,10 @@ public class PingPong {
     for (Integer index = 1 ; index < userInput + 1; index ++) {
         if(index % 3 == 0){
           pingPongArray.add("ping");
+        } else if(index % 5 == 0){
+          pingPongArray.add("pong");
         } else {
-          pingPongArray.add(index);
+        pingPongArray.add(index);
         }
       }
     return pingPongArray;
